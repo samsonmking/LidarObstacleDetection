fa6c36feb475a394c5ae95eca2a13c2acac1f598
samsonmking <samson@samsonmking.com>
2021-05-15 16:42:02 -0400

    Implement manual clustering with KD-Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -3,56 +3,73 @@
 
 #include "../../render/render.h"
 
-
 // Structure to represent node of kd tree
-struct Node
-{
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+struct Node {
+  std::vector<float> point;
+  int id;
+  Node* left;
+  Node* right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+  Node(std::vector<float> arr, int setId) : point(arr), id(setId), left(NULL), right(NULL) {}
 
-	~Node()
-	{
-		delete left;
-		delete right;
-	}
+  ~Node() {
+    delete left;
+    delete right;
+  }
 };
 
-struct KdTree
-{
-	Node* root;
+struct KdTree {
+  Node* root;
 
-	KdTree()
-	: root(NULL)
-	{}
+  KdTree() : root(NULL) {}
 
-	~KdTree()
-	{
-		delete root;
-	}
+  ~KdTree() { delete root; }
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+  void insert(std::vector<float> point, int id, int depth = 0) { innerInsert(point, id, 0, root); }
 
-	}
+  void innerInsert(std::vector<float> point, int id, int depth, Node*& currentNode) {
+    if (currentNode == NULL) {
+      currentNode = new Node(point, id);
+      return;
+    }
 
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
+    int comp = depth % 2;
+    float root_value = currentNode->point[comp];
+    float point_value = point[comp];
 
+    if (point_value < root_value) {
+      innerInsert(point, id, ++depth, currentNode->left);
+    } else {
+      innerInsert(point, id, ++depth, currentNode->right);
+    }
+  }
+
+  // return a list of point ids in the tree that are within distance of target
+  std::vector<int> search(std::vector<float> target, float distanceTol) {
+    std::vector<int> ids;
+    innerSearch(target, distanceTol, ids, root, 0);
+    return ids;
+  }
+
+  void innerSearch(std::vector<float> target, float distanceTol, std::vector<int>& ids, Node*& currentNode, int depth) {
+    if (currentNode == NULL) {
+      return;
+    }
+    if (pointInBox(target, currentNode->point, distanceTol)) {
+      ids.push_back(currentNode->id);
+    }
+    int comp = depth % 2;
+    if ((target[comp] - distanceTol) < currentNode->point[comp]) {
+      innerSearch(target, distanceTol, ids, currentNode->left, depth + 1);
+    }
+    if ((target[comp] + distanceTol) > currentNode->point[comp]) {
+      innerSearch(target, distanceTol, ids, currentNode->right, depth + 1);
+    }
+  }
+
+  bool pointInBox(std::vector<float>& target, std::vector<float>& point, float distanceTol) {
+    float xDist = fabs(target[0] - point[0]);
+    float yDist = fabs(target[1] - point[1]);
+    return xDist <= distanceTol && yDist <= distanceTol && sqrt(pow(xDist, 2) + pow(yDist, 2)) < distanceTol;
+  }
 };
-
-
-
-
